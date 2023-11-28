a122f3d85d9d2c131b354b29f40eb5ec225d4535
william <nixon@williams-MacBook-Air.local>
2023-11-28 23:50:12 +0700

    update webhook

--- payment-app/src/app.js
+++ payment-app/src/app.js
@@ -12,7 +12,6 @@ const queueName = 'invoice_queue';
 
 app.use(bodyParser.json());
 
-const TICKET_WEBHOOK_URL = process.env.TICKET_WEBHOOK_URL || 'http://localhost:4000/webhook';
 const CALLBACK_WEBHOOK_URL = process.env.CALLBACK_WEBHOOK_URL || 'http://localhost:5000/webhook';
 
 rabbitMQ.setConsumer(queueName, processQueueMessage);
@@ -20,19 +19,19 @@ rabbitMQ.createChannel();
 
 app.post('/invoice', async (req, res) => {
     try {
-        const { amount, customer_name } = req.body;
+        const { amount, bookingId } = req.body;
 
         const invoiceId = uuid.v4();
-        const webhookUrl = `${CALLBACK_WEBHOOK_URL}?invoiceId=${invoiceId}`;
+        const webhookUrl = `${CALLBACK_WEBHOOK_URL}?invoiceId=${invoiceId}&bookingId=${bookingId}`;
 
         await Invoice.create({
             amount,
-            customer_name,
+            bookingId,
             invoiceId,
             webhookUrl,
         });
 
-        res.json({ invoiceId, webhookUrl });
+        res.json({ invoiceId, bookingId, webhookUrl });
     } catch (error) {
         console.error('Error:', error);
         res.status(500).send('Internal Server Error');
@@ -42,12 +41,14 @@ app.post('/invoice', async (req, res) => {
 
 app.get('/webhook', async (req, res) => {
     try {
-        const { invoiceId } = req.query;
-        const dataToEnqueue = { invoiceId, timestamp: new Date() };
+        const { invoiceId, bookingId } = req.query;
+        const dataToEnqueue = { invoiceId, bookingId, timestamp: new Date() };
 
         const rabbitMQChannel = await rabbitMQ.getChannel();
         await rabbitMQChannel.assertQueue(queueName, { durable: true, persistent: true });
         rabbitMQChannel.sendToQueue(queueName, Buffer.from(JSON.stringify(dataToEnqueue)), { persistent: true });
+
+
 
         console.log(`Webhook request received for invoice ID: ${invoiceId}, data enqueued for processing`);
         res.status(200).send('Webhook request received');
--- payment-app/src/utils.js
+++ payment-app/src/utils.js
@@ -1,7 +1,6 @@
 const { Log } = require('./db'); // Adjust the path as needed
 
 const TICKET_WEBHOOK_URL = process.env.TICKET_WEBHOOK_URL || 'http://localhost:4000/webhook';
-const CALLBACK_WEBHOOK_URL = process.env.CALLBACK_WEBHOOK_URL || 'http://localhost:5000/webhook';
 
 // Ganti jadi 0.1 nanti
 function simulatePayment() {
@@ -10,22 +9,28 @@ function simulatePayment() {
 }
 
 async function processQueueMessage(msg) {
-    await new Promise(resolve => setTimeout(resolve, 5000));
+    // Simulate payment process timeout 2 second
+    await new Promise(resolve => setTimeout(resolve, 2000));
+    const { invoiceId, bookingId } = JSON.parse(msg.content);
 
-    const invoiceId = JSON.parse(msg.content).invoiceId;
-    console.log("Simulating payment process...")
+    console.log("Simulating payment process with 10% failure...")
     const isSuccessful = simulatePayment();
-    const webhookUrl = `${TICKET_WEBHOOK_URL}?invoiceId=${invoiceId}&status=${isSuccessful ? 'success' : 'failure'}`;
-    console.log(`Result for invoice ID: ${invoiceId}, Payment Status: ${isSuccessful ? 'Success' : 'Failure'}`)
+    const webhookUrl = `${TICKET_WEBHOOK_URL}?invoiceId=${invoiceId}&bookingId=${bookingId}&success=${isSuccessful ? 'true' : 'false'}`;
+
+    console.log(`Result for invoice ID: ${invoiceId}, booking ID: ${bookingId}, payment Status: ${isSuccessful ? 'Success' : 'Failure'}`)
+
     try {
         const status = isSuccessful ? 'success' : 'failure';
         await Log.create({
             invoiceId,
+            bookingId,
             status,
             timestamp: new Date(),
         });
         console.log('Log entry saved in the database.');
         console.log(`Calling the success/fail webhook: ${webhookUrl}`);
+        await fetch(webhookUrl, { method: 'GET' });
+        console.log('Webhook called successfully.');
     } catch (error) {
         console.error('Error in sending the webhook! ');
         throw (error);
